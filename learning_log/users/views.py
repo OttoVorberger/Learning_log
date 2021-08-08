afb8ab5c200d11d2c2e23e6dd4756f7ed114b1cb
Leonid <cqcqdeut2gx@gmail.com>
2021-08-08 14:23:15 +0300

    Fixed user registration.

--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -12,6 +12,6 @@ def register(request):
         if form.is_valid():
             new_user = form.save()
             login(request, new_user)
-            return redirect('learning_logs:index')
+            return redirect('index')
     context = {'form': form}
     return render(request, 'registration/register.html', context)
